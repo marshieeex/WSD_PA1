744dec3a18251dda40d4d9e42289262c7f511682
NURMARSYITA BTE <nurmarsyita@connect.ite.edu.sg>
2021-05-04 12:48:40 +0800

    Add project files.

---
+++ WebsitePrototype/Controllers/ServicesController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace WebsitePrototype.Controllers
+{
+    public class ServicesController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+	}
+}
